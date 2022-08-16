d161c02cb966843ffa67cbb26f60b07691808d5a
Ariful Islam <Ariful Islam>
2022-08-16 21:51:43 +0600

    make some change

--- jsArray/03.Array-CRUD/lecture1.js
+++ jsArray/03.Array-CRUD/lecture1.js
@@ -1,4 +1,13 @@
 /**
- * Title
+ * @Title: Add New Elements
  */
-const arr = [];
+
+// Use push to insert at the end
+
+const arrP1 = [1, 2, 3];
+const arrP2 = [8, 9];
+
+arrP1.push(5);
+arrP1.push(6, 7);
+
+console.log(arrP1);
